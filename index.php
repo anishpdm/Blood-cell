172d23d726f9b1f33363fa54b3b9ca9bbb62ae10
anishpdm <anish.vilayil.s@gmail.com>
2022-07-07 15:10:58 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body style="background: red">
+
+    <nav class="navbar navbar-expand-sm bg-success navbar-dark">
+        <ul class="navbar-nav">
+
+            <li class="nav-item active">
+                <a class="nav-link" href="cancerpredict.php">Blood cell count </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">LogOut</a>
+            </li>
+
+        </ul>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col col-sm-2">
+
+
+            </div>
+
+            <div class="col col-sm-8 col-12">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <table class="table">
+                        <tr>
+                            <td></td>
+                            <td>
+                                <h4> Bloodcell Image </h4>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Upload Blood Cell image </td>
+                            <td>
+                                <input class="form-control" type="file" name="fileToUpload" id="fileToUpload">
+                            </td>
+                        </tr>
+
+
+                        <tr>
+                            <td></td>
+                            <td><button class="btn btn-success" name="but" type="submit">CHECK</button></td>
+                        </tr>
+
+                        <br>
+                        <br>
+
+
+                        <tr>
+                            <td></td>
+                            <td>
+
+                            </td>
+                        </tr>
+
+
+
+
+                    </table>
+                </form>
+
+            </div>
+
+
+            <div class="col col-sm-2">
+
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
+
+
+
+<?php
+include './db.php';
+if (isset($_POST["but"])) {
+
+    $target_dir = "uploads/";
+    $target_file = $target_dir . "testimage.jpg";
+
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        // echo "File Uploaded Succesfully";
+
+        $res = file_get_contents("http://127.0.0.1:4000/predict");
+        $obj = json_decode($res);
+        $rbc = $obj->{'rbc'};
+        $wbc = $obj->{'wbc'};
+        echo "<center> <p class='text-light'> Red Blood cell count : $rbc   </p> </center> <br>";
+        echo "<center><p class='text-light'> White Blood cell count : $wbc </p> </center>";
+
+
+    }
+
+
+}
+
+
+?>
---
+++ predict.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, url_for, request, jsonify
+
+import numpy as np 
+import cv2 
+
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/predict")
+def image():
+    image = cv2.imread('./uploads/testimage.jpg')
+
+    # Convert it to HSV Color Map
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # Set the thresholds for RBC Detection
+    rlower = np.array([160, 60, 100])
+    rupper = np.array([180, 200, 255])
+
+    # Find the colors within the specified boundaries and apply the mask
+    rmask = cv2.inRange(img, rlower, rupper)
+
+    # Find the contours
+    rcontours, hierarchy = cv2.findContours(rmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    # Count the number of RBC's
+    rbc_count = 0
+    rbc = []
+    multiple_cells = []
+    for contour in rcontours:
+        a = cv2.contourArea(contour) 
+        if a > 75 and a <= 300:
+            rbc.append(contour)
+            rbc_count += 1
+        elif a > 300:
+            multiple_cells.append(contour)
+            if a <= 600:
+                rbc_count += 2
+            else:
+                rbc_count += 3
+
+    # Print the count 
+    print(f'Approximate number of RBC: {rbc_count}')
+
+    # Segment the RBC's
+    cv2.drawContours(image, multiple_cells, -1, (0, 0, 255), 3)
+    cv2.drawContours(image, rbc, -1, (0, 0, 255), 3)
+
+
+    # Set the thresholds for WBC Detection
+    wlower = np.array([120, 50, 20])
+    wupper = np.array([140, 255, 255])
+
+    # Find the colors within the specified boundaries and apply the mask
+    wmask = cv2.inRange(img, wlower, wupper)
+
+    # Find the contours
+    wcontours, hierarchy = cv2.findContours(wmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    # Count and segment the WBC's
+    cv2.drawContours(image, wcontours, -1, (255, 0, 0), 3)
+    wbc_count = len(wcontours)
+
+    # Print the count
+    print(f'Approximate number of WBC: {wbc_count}')
+
+    # Write the output in a file
+    cv2.imwrite('op.jpg', image)
+    
+
+    return jsonify({"rbc": rbc_count, "wbc": wbc_count})
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0", port=4000)
